48c1cb05725cd4d55bdde1c297c3d1ce7cc6347c
Rodrigo Diaz <pericodiaz89@gmail.com>
2024-11-03 16:59:39 +0100

    Revert "test"
    This reverts commit e1edfb09b20b8bbe7f5391f27865ca02162176e6.

--- medialingua/services/progressService.js
+++
@@ -1,8 +0,0 @@
-export const saveProgress = (progress) => {
-    localStorage.setItem('progress', JSON.stringify(progress));
-  };
-  
-  export const loadProgress = () => {
-    const progress = localStorage.getItem('progress');
-    return progress ? JSON.parse(progress) : [];
-  };
--- medialingua/services/vocabularyService.js
+++
@@ -1,7 +0,0 @@
-import nlp from 'compromise';
-
-export const extractVocabulary = (text) => {
-  const doc = nlp(text);
-  const words = doc.nouns().out('array');
-  return words;
-};
